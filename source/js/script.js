70ecb5342a73c2eec2f6fb93b35749ad2b3455e2
Nikita <vinsmir@mail.ru>
2023-05-26 02:50:26 +0300

    styles hero block + mobile

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,23 @@
+const navMain = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+const navToggleClose = document.querySelector('.nav__close-toggle');
+
+navMain.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--opened');
+  }
+});
+
+navToggleClose.addEventListener('click', function () {
+  if (navMain.classList.contains('nav--opened')) {
+    navMain.classList.add('nav--closed');
+    navMain.classList.remove('nav--opened');
+  }
+});
+
 // swiper
 const swiper = new Swiper(".mySwiper", {
   navigation: {
